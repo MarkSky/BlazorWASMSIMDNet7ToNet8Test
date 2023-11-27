e81fe7939ca59929df0b41adabe27b569c85d186
MarkSky <uxmark@outlook.com>
2023-11-27 10:36:30 +0800

    加入專案檔案。

---
+++ BlazorWASMSIMDNet7ToNet8/Client/Program.cs
@@ -0,0 +1,17 @@
+using BlazorWASMSIMDNet7ToNet8.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorWASMSIMDNet7ToNet8.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWASMSIMDNet7ToNet8.ServerAPI"));
+
+builder.Services.AddPWAUpdater();
+
+await builder.Build().RunAsync();
